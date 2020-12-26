9791504faaa2aeec60938b4470b23ab3c064963a
gagan1393 <gagan1393dp@gmail.com>
2020-12-26 21:45:39 +0530

    screenshot capture testcase

---
+++ screenshot.java
@@ -0,0 +1,40 @@
+package capturecreenshot;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+
+public class screenshot 
+{
+	public static WebDriver driver;
+	public static void main(ITestResult result)  {
+
+		// Here will compare if test is failing then only it will enter into if condition
+		if(ITestResult.FAILURE==result.getStatus())
+		{
+			try 
+			{
+				// Create refernce of TakesScreenshot
+				TakesScreenshot ts=(TakesScreenshot)driver;
+
+				// Call method to capture screenshot
+				File source=ts.getScreenshotAs(OutputType.FILE);
+
+				// Copy files to specific location here it will save all screenshot in our project home directory and
+				// result.getName() will return name of test case so that screenshot name will be same
+
+				FileUtils.copyFile(source, new File("./Screenshots/"+result.getName()+".png"));
+
+				System.out.println("Screenshot taken");
+			} 
+			catch (Exception e)
+			{
+
+				System.out.println("Exception while taking screenshot "+e.getMessage());
+			} 
+		}
+	}}
